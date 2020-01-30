e5bfac344392b616c51ae7c58026ae7e55f45b4a
PJ <peterjan.brone@gmail.com>
2020-01-30 12:49:30 +0100

    Update url

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Nebulous SDK",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://https://gitlab.com/pjbrone/python-nebulous",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
