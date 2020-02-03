eb4e3aece45f9a74eef05f0c2d2b57cbdf842424
PJ <peterjan.brone@gmail.com>
2020-02-03 19:28:04 +0100

    Use sia:// prefix

--- siaskynet/skynet.py
+++ siaskynet/skynet.py
@@ -17,6 +17,12 @@ class Skynet:
         })
 
     @staticmethod
+    def strip_prefix(str):
+        if str.startswith("sia://"):
+            return str[len("sia://"):]
+        return str
+
+    @staticmethod
     def UploadFile(path, opts=None):
         if opts is None:
             opts = Skynet.default_upload_options()
@@ -24,12 +30,12 @@ class Skynet:
         with open(path, 'rb') as f:
             r = requests.post("%s%s" % (opts.portalUrl, opts.portalUploadPath), files={opts.portalFileFieldname: f})
             response = r.json()
-            return response["skylink"]
+            return "sia://" + response["skylink"]
 
     @staticmethod
     def DownloadFile(path, skylink, opts=None):
         if opts is None:
             opts = Skynet.default_download_options()
 
-        r = requests.get("%s/%s" % (opts.portalUrl, skylink),allow_redirects=True)
+        r = requests.get("%s/%s" % (opts.portalUrl, Skynet.strip_prefix(skylink)),allow_redirects=True)
         open(path, 'wb').write(r.content)
