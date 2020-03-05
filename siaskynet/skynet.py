3f4cb67dc328e2e737278dd27145de12fa247a24
Kcchouette <Kcchouette@users.noreply.github.com>
2020-03-05 13:44:45 +0100

    do the same for variables

--- siaskynet/skynet.py
+++ siaskynet/skynet.py
@@ -9,11 +9,11 @@ class Skynet:
     @staticmethod
     def default_upload_options():
         return type('obj', (object,), {
-            'portalUrl': 'https://siasky.net',
-            'portalUploadPath': 'skynet/skyfile',
-            'portalFileFieldname': 'file',
-            'portalDirectoryFileFieldname': 'files[]',
-            'customFilename': ''
+            'portal_url': 'https://siasky.net',
+            'portal_upload_path': 'skynet/skyfile',
+            'portal_file_fieldname': 'file',
+            'portal_directory_file_fieldname': 'files[]',
+            'custom_filename': ''
         })
 
     @staticmethod
@@ -30,7 +30,7 @@ class Skynet:
 
     @staticmethod
     def upload_file(path, opts=None):
-        return "sia://" + Skynet.UploadFileRequest(path, opts).json()["skylink"]
+        return "sia://" + Skynet.upload_file_request(path, opts).json()["skylink"]
 
     @staticmethod
     def upload_file_request(path, opts=None):
@@ -41,15 +41,15 @@ class Skynet:
         uuid = ''.join(random.choice(charset) for i in range(16))
 
         with open(path, 'rb') as f:
-            host = opts.portalUrl
-            path = opts.portalUploadPath
+            host = opts.portal_url
+            path = opts.portal_upload_path
             url = f'{host}/{path}/{uuid}'
-            r = requests.post(url, files={opts.portalFileFieldname: f})
+            r = requests.post(url, files={opts.portal_file_fieldname: f})
         return r
 
     @staticmethod
     def upload_directory(path, opts=None):
-        return "sia://" + Skynet.UploadDirectoryRequest(path, opts).json()["skylink"]
+        return "sia://" + Skynet.upload_directory_request(path, opts).json()["skylink"]
 
     @staticmethod
     def upload_directory_request(path, opts=None):
@@ -61,25 +61,25 @@ class Skynet:
             opts = Skynet.default_upload_options()
 
         ftuples = []
-        files = list(Skynet.walkDirectory(path).keys())
+        files = list(Skynet.walk_directory(path).keys())
         for file in files:
-            ftuples.append((opts.portalDirectoryFileFieldname,
+            ftuples.append((opts.portal_directory_file_fieldname,
                             (file, open(file, 'rb'))))
 
         charset = string.ascii_lowercase
         uuid = ''.join(random.choice(charset) for i in range(16))
 
-        filename = opts.customFilename if opts.customFilename else path
+        filename = opts.custom_filename if opts.custom_filename else path
 
-        host = opts.portalUrl
-        path = opts.portalUploadPath
+        host = opts.portal_url
+        path = opts.portal_upload_path
         url = f'{host}/{path}/{uuid}?filename={filename}'
         r = requests.post(url, files=ftuples)
         return r
 
     @staticmethod
     def download_file(path, skylink, opts=None):
-        r = Skynet.DownloadFileRequest(skylink, opts)
+        r = Skynet.download_file_request(skylink, opts)
         open(path, 'wb').write(r.content)
 
     @staticmethod
@@ -87,7 +87,7 @@ class Skynet:
         if opts is None:
             opts = Skynet.default_download_options()
 
-        portal = opts.portalUrl
+        portal = opts.portal_url
         skylink = Skynet.strip_prefix(skylink)
         url = f'{portal}/{skylink}'
         r = requests.get(url, allow_redirects=True)
@@ -98,7 +98,7 @@ class Skynet:
         files = {}
         for root, subdirs, subfiles in os.walk(path):
             for subdir in subdirs:
-                files.update(Skynet.walkDirectory(os.path.join(root, subdir)))
+                files.update(Skynet.walk_directory(os.path.join(root, subdir)))
             for subfile in subfiles:
                 fullpath = os.path.join(root, subfile)
                 files[fullpath] = True
