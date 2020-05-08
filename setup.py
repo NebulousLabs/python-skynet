b53714471e63bd9dc64fe48571d834b6d5927586
Karl Semich <0xloem@gmail.com>
2020-05-08 10:05:20 -0400

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="siaskynet",
-    version="1.0.2",
+    version="1.0.3",
     author="Peter-Jan Brone",
     author_email="peterjan.brone@gmail.com",
     description="Skynet SDK",
