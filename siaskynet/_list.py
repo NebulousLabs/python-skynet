3ce5ddcdebe06c861f186bc23b67a1af5a483e1b
Marcin S <scatman@bu.edu>
2020-08-14 19:01:57 +0200

    Fix CI error

--- siaskynet/_list.py
+++ siaskynet/_list.py
@@ -5,12 +5,12 @@
 from . import utils
 
 
-def default_ls_options():
-    """Returns the default ls options."""
+def default_list_files_options():
+    """Returns the default list files options."""
 
     obj = utils.default_options("")
-    obj['endpoint_path_ls_dir'] = "renter/dir"
-    obj['endpoint_path_ls_file'] = "renter/file"
+    obj['endpoint_path_list_files_dir'] = "renter/dir"
+    obj['endpoint_path_list_files_file'] = "renter/file"
 
     return obj
 
